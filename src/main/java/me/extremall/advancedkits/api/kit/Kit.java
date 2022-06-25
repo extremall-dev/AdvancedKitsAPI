fe763775129d225c6d216fb709ad09f1b979aeb2
extremall-dev <94036944+extremall-dev@users.noreply.github.com>
2022-06-25 09:42:29 +0300

    Add files via upload

---
+++ src/main/java/me/extremall/advancedkits/api/kit/Kit.java
@@ -0,0 +1,20 @@
+package me.extremall.advancedkits.api.kit;
+
+import me.extremall.advancedkits.menu.preview.PreviewMenu;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+
+public interface Kit
+{
+    @NotNull String getIdentifier();
+    @NotNull String getPermission();
+    @NotNull Map<Integer, KitLevel> getLevels();
+    @Nullable KitLevel getLevel(int level);
+    boolean isUpgradeable();
+
+    boolean isValidLevel(int level);
+
+    PreviewMenu getPreviewMenu();
+}
