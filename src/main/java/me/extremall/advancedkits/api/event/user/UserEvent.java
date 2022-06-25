8d35ebeb9bab058ae7f667d8d18387ac0c71d05e
extremall-dev <94036944+extremall-dev@users.noreply.github.com>
2022-06-25 09:48:55 +0300

    Add files via upload

---
+++ src/main/java/me/extremall/advancedkits/api/event/user/UserEvent.java
@@ -0,0 +1,21 @@
+package me.extremall.advancedkits.api.event.user;
+
+import me.extremall.advancedkits.api.user.User;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class UserEvent extends Event
+{
+    protected User user;
+
+    public UserEvent(@NotNull User user)
+    {
+        this.user = user;
+    }
+
+    @NotNull
+    public final User getUser()
+    {
+        return this.user;
+    }
+}
