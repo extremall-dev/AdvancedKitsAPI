e10612f4566c64543f3f0592e41ee444c90433fa
extremall-dev <94036944+extremall-dev@users.noreply.github.com>
2022-07-10 23:40:40 +0300

    Create UserDatabase.java

---
+++ src/main/java/me/extremall/advancedkits/api/user/UserDatabase.java
@@ -0,0 +1,33 @@
+package me.extremall.advancedkits.api.user;
+
+import me.extremall.advancedkits.api.kit.Kit;
+import org.jetbrains.annotations.NotNull;
+
+public interface UserDatabase
+{
+    int getKitLevel(@NotNull Kit kit);
+
+    boolean isSetKitLevel(@NotNull Kit kit);
+
+    void setKitLevel(@NotNull Kit kit, int level);
+
+    long getKitCooldown(@NotNull Kit kit);
+
+    boolean isSetKitCooldown(@NotNull Kit kit);
+
+    void setKitCooldown(@NotNull Kit kit, long delay);
+
+    int getKitUsages(@NotNull Kit kit);
+
+    boolean isSetKitUsages(@NotNull Kit kit);
+
+    void setKitUsages(@NotNull Kit kit, int usages);
+
+    boolean exists();
+
+    void load();
+
+    void save();
+
+    void reset();
+}
