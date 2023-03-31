d40f973ca98ae0202c3656c353e09fbcf9dbfedb
extremall-dev <94036944+extremall-dev@users.noreply.github.com>
2023-03-31 21:20:42 +0300

    Update UpgradeKitEvent.java

--- src/main/java/me/extremall/advancedkits/api/event/user/UpgradeKitEvent.java
+++ src/main/java/me/extremall/advancedkits/api/event/user/UpgradeKitEvent.java
@@ -1,6 +1,5 @@
 package me.extremall.advancedkits.api.event.user;
 
-import me.extremall.advancedkits.api.kit.Kit;
 import me.extremall.advancedkits.api.kit.KitLevel;
 import me.extremall.advancedkits.api.user.User;
 import org.bukkit.event.Cancellable;
@@ -8,21 +7,13 @@ import org.jetbrains.annotations.NotNull;
 
 public class UpgradeKitEvent extends BaseUserEvent implements Cancellable
 {
-    private final Kit kit;
     private final KitLevel newKitLevel;
     private boolean cancelled;
 
-    public UpgradeKitEvent(@NotNull User user, @NotNull Kit kit, @NotNull KitLevel newKitLevel)
+    public UpgradeKitEvent(@NotNull User user, @NotNull KitLevel newKitLevel)
     {
         super(user);
-        this.kit = kit;
         this.newKitLevel = newKitLevel;
-    }
-
-    @NotNull
-    public Kit getKit()
-    {
-        return this.kit;
     }
 
     @NotNull
