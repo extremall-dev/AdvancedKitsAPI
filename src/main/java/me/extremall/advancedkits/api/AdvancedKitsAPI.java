fd8e807ac85dabbd8b234e582ee4cb05c5f3d9fb
extremall-dev <94036944+extremall-dev@users.noreply.github.com>
2023-03-31 21:22:52 +0300

    Update AdvancedKitsAPI.java

--- src/main/java/me/extremall/advancedkits/api/AdvancedKitsAPI.java
+++ src/main/java/me/extremall/advancedkits/api/AdvancedKitsAPI.java
@@ -1,15 +1,23 @@
 package me.extremall.advancedkits.api;
 
+import me.extremall.advancedkits.api.kit.KitManager;
 import me.extremall.advancedkits.api.user.UserManager;
 import org.jetbrains.annotations.NotNull;
 
 public class AdvancedKitsAPI
 {
     private static UserManager userManager;
+    private static KitManager kitManager;
 
     @NotNull
     public static UserManager getUserManager()
     {
         return AdvancedKitsAPI.userManager;
+    }
+
+    @NotNull
+    public static KitManager getKitManager()
+    {
+        return AdvancedKitsAPI.kitManager;
     }
 }
