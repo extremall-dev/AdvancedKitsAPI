fe1b163410e3eede0718aab2a9bb3949b9698a40
extremall-dev <94036944+extremall-dev@users.noreply.github.com>
2023-03-31 21:20:16 +0300

    Update ReceiveKitEvent.java

--- src/main/java/me/extremall/advancedkits/api/event/user/ReceiveKitEvent.java
+++ src/main/java/me/extremall/advancedkits/api/event/user/ReceiveKitEvent.java
@@ -1,6 +1,5 @@
 package me.extremall.advancedkits.api.event.user;
 
-import me.extremall.advancedkits.api.kit.Kit;
 import me.extremall.advancedkits.api.kit.KitLevel;
 import me.extremall.advancedkits.api.kit.KitStatus;
 import me.extremall.advancedkits.api.user.User;
@@ -10,24 +9,12 @@ import org.jetbrains.annotations.NotNull;
 public class ReceiveKitEvent extends BaseUserEvent implements Cancellable
 {
     private boolean cancelled;
-    private Kit kit;
     private KitLevel kitLevel;
     private KitStatus kitStatus;
 
     public ReceiveKitEvent(@NotNull User user)
     {
         super(user);
-    }
-
-    @NotNull
-    public Kit getKit()
-    {
-        return this.kit;
-    }
-
-    public void setKit(@NotNull Kit kit)
-    {
-        this.kit = kit;
     }
 
     @NotNull
