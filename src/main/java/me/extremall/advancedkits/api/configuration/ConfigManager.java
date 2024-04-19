cc39523ecc8431cfa35d8ec8d530ca7d00772744
extremall-dev <94036944+extremall-dev@users.noreply.github.com>
2024-04-19 07:56:54 +0300

    Delete src/main/java/me/extremall/advancedkits directory

--- src/main/java/me/extremall/advancedkits/api/configuration/ConfigManager.java
+++
@@ -1,9 +0,0 @@
-package me.extremall.advancedkits.api.configuration;
-
-import dev.dejvokep.boostedyaml.YamlDocument;
-import org.jetbrains.annotations.NotNull;
-
-public interface ConfigManager
-{
-    @NotNull YamlDocument getFile(@NotNull ConfigFile key);
-}
