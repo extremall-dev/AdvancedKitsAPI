cdee6a36c90bda923fec86deac28bfa168b91a98
extremall-dev <94036944+extremall-dev@users.noreply.github.com>
2026-01-15 21:14:36 +0200

    Update GrantTriggerEvent.java

--- src/main/java/me/extremall/advancedkits/api/event/user/GrantTriggerEvent.java
+++ src/main/java/me/extremall/advancedkits/api/event/user/GrantTriggerEvent.java
@@ -2,13 +2,11 @@ package me.extremall.advancedkits.api.event.user;
 
 import lombok.Getter;
 import lombok.Setter;
-import me.extremall.advancedkits.api.trigger.GrantTrigger;
+import me.extremall.advancedkits.api.trigger.GrantTriggerContext;
 import me.extremall.advancedkits.api.trigger.GrantTriggerType;
 import me.extremall.advancedkits.api.user.User;
 import org.bukkit.event.Cancellable;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
 
 @Getter
 public class GrantTriggerEvent extends BaseUserEvent implements Cancellable
@@ -16,12 +14,12 @@ public class GrantTriggerEvent extends BaseUserEvent implements Cancellable
     @Setter
     private boolean cancelled;
     private final GrantTriggerType grantTriggerType;
-    private final List<GrantTrigger> grantTriggers;
+    private final GrantTriggerContext grantTriggerContext;
 
-    public GrantTriggerEvent(@NotNull User user, @NotNull GrantTriggerType grantTriggerType, @NotNull List<GrantTrigger> grantTriggers)
+    public GrantTriggerEvent(@NotNull User user, @NotNull GrantTriggerType grantTriggerType, @NotNull GrantTriggerContext grantTriggerContext)
     {
         super(user);
         this.grantTriggerType = grantTriggerType;
-        this.grantTriggers = grantTriggers;
+        this.grantTriggerContext = grantTriggerContext;
     }
 }
